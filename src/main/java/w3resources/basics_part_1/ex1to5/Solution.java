a473aab172aac6be6958107d05c319c087254e9e
Eduardo Botelho <eduardo11224b@gmail.com>
2025-08-31 18:35:39 +0100

    basics part 1 exercise 1

---
+++ src/main/java/w3resources/basics_part_1/ex1to5/Solution.java
@@ -0,0 +1,11 @@
+package w3resources.basics_part_1.ex1to5;
+
+public class Solution {
+    /**
+     * Prints hello, and then my name.
+     */
+    public void helloAndNamePrinter(){
+        System.out.println("Hello");
+        System.out.println("Ace");
+    }
+}
